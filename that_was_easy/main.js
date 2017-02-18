606215cb2dbbbb842601e904d386475403b08456
Jacob Neale <cedricstrobe@outlook.com>
2017-02-18 00:48:18 +0000

    Create that was easy button

---
+++ that_was_easy/main.js
@@ -0,0 +1,9 @@
+var jujuOnThatBeat = new Audio ("Juju.mp3");
+    jujuOnThatBeat.play();
+    
+function sayThatWasEasy() {
+    var thatWasEasy = new Audio ("that_was_easy.mp3");
+    thatWasEasy.play();
+}
+
+$("#easy").on("click", sayThatWasEasy);
